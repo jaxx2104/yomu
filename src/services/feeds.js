ddae33183f54650ef3251b450d69082ef3e85c6a
jaxx2104 <jaxx2104@gmail.com>
2018-10-07 04:13:39 +0900

    Update services

--- src/services/feeds.js
+++ src/services/feeds.js
@@ -2,15 +2,25 @@ import axios from "axios"
 import qs from "querystring"
 import rss from "~/src/assets/rss.json"
 
-export const getFeed = url => {
-  const baseUrl = "https://query.yahooapis.com/v1/public/yql?"
+export const get = url => {
+  const baseUrl = "api/v3/streams/contents"
   const options = qs.stringify({
-    q: `select * from feednormalizer where output="atom_1.0" AND url in ('${url}')`,
-    format: "json"
+    count: 10,
+    streamId: "feed"
   })
-  return axios.get(`${baseUrl}${options}`)
+  const query = encodeURIComponent(url)
+  return axios.get(`${baseUrl}?${options}/${query}`)
 }
 
-export const getFeedDummy = () => {
+export const getDummy = () => {
   return Promise.resolve(rss)
+}
+
+export const search = query => {
+  const baseUrl = "api/v3/search/feeds"
+  const options = qs.stringify({
+    count: 10,
+    query: query
+  })
+  return axios.get(`${baseUrl}?${options}`)
 }
