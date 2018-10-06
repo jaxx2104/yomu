9e3b6c1401727c4037cfd7b77d155fe05d741cea
jaxx2104 <jaxx2104@gmail.com>
2018-10-07 04:43:03 +0900

    Update pwacompat

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,46 +16,16 @@ module.exports = {
     link: [
       { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
       {
-        rel: "apple-touch-startup-image",
-        href: "/images/splash/launch-640x1136.png",
-        media:
-          "(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-      },
+        rel: "manifest",
+        href: "manifest.webmanifest"
+      }
+    ],
+    script: [
       {
-        rel: "apple-touch-startup-image",
-        href: "/images/splash/launch-750x1294.png",
-        media:
-          "(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-      },
-      {
-        rel: "apple-touch-startup-image",
-        href: "/images/splash/launch-1242x2148.png",
-        media:
-          "(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-      },
-      {
-        rel: "apple-touch-startup-image",
-        href: "/images/splash/launch-1125x2436.png",
-        media:
-          "(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-      },
-      {
-        rel: "apple-touch-startup-image",
-        href: "/images/splash/launch-1536x2048.png",
-        media:
-          "(min-device-width: 768px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"
-      },
-      {
-        rel: "apple-touch-startup-image",
-        href: "/images/splash/launch-1668x2224.png",
-        media:
-          "(min-device-width: 834px) and (max-device-width: 834px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"
-      },
-      {
-        rel: "apple-touch-startup-image",
-        href: "/images/splash/launch-2048x2732.png",
-        media:
-          "(min-device-width: 1024px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"
+        src: "https://cdn.jsdelivr.net/npm/pwacompat@2.0.6/pwacompat.min.js",
+        integrity:
+          "sha384-GOaSLecPIMCJksN83HLuYf9FToOiQ2Df0+0ntv7ey8zjUHESXhthwvq9hXAZTifA",
+        crossorigin: "anonymous"
       }
     ]
   },
