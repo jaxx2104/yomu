2828319fa08a19112f21af2a7db003b9d11021f5
Futoshi Iwashita <jaxx2104@gmail.com>
2018-10-08 17:55:03 +0900

    Update pwacompat (#56)
    * Update pwacompat
    
    * Delete image
    
    * manifest の記述を nuxt にさせる

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,49 +13,13 @@ module.exports = {
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { hid: "description", name: "description", content: "Nuxt.js project" }
     ],
-    link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    script: [
       {
-        rel: "apple-touch-startup-image",
-        href: "/images/splash/launch-640x1136.png",
-        media:
-          "(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-      },
-      {
-        rel: "apple-touch-startup-image",
-        href: "/images/splash/launch-750x1294.png",
-        media:
-          "(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-      },
-      {
-        rel: "apple-touch-startup-image",
-        href: "/images/splash/launch-1242x2148.png",
-        media:
-          "(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-      },
-      {
-        rel: "apple-touch-startup-image",
-        href: "/images/splash/launch-1125x2436.png",
-        media:
-          "(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-      },
-      {
-        rel: "apple-touch-startup-image",
-        href: "/images/splash/launch-1536x2048.png",
-        media:
-          "(min-device-width: 768px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"
-      },
-      {
-        rel: "apple-touch-startup-image",
-        href: "/images/splash/launch-1668x2224.png",
-        media:
-          "(min-device-width: 834px) and (max-device-width: 834px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"
-      },
-      {
-        rel: "apple-touch-startup-image",
-        href: "/images/splash/launch-2048x2732.png",
-        media:
-          "(min-device-width: 1024px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"
+        src: "https://cdn.jsdelivr.net/npm/pwacompat@2.0.6/pwacompat.min.js",
+        integrity:
+          "sha384-GOaSLecPIMCJksN83HLuYf9FToOiQ2Df0+0ntv7ey8zjUHESXhthwvq9hXAZTifA",
+        crossorigin: "anonymous"
       }
     ]
   },
