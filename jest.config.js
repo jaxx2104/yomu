c56221bd9c11fba8292c118077465fdb4123db9f
jaxx2104 <jaxx2104@gmail.com>
2019-05-19 16:50:30 +0900

    remove nuxt

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,27 @@
 module.exports = {
-  moduleFileExtensions: ["js", "json", "vue"],
+  moduleFileExtensions: ["js", "jsx", "json", "vue", "ts", "tsx"],
   transform: {
-    ".*\\.(vue)$": "vue-jest",
-    "^.+\\.js$": "<rootDir>/node_modules/babel-jest"
+    "^.+\\.vue$": "vue-jest",
+    ".+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$":
+      "jest-transform-stub",
+    "^.+\\.tsx?$": "ts-jest"
   },
-  collectCoverageFrom: ["**/*.{js,vue}", "!**/node_modules/**"],
-  snapshotSerializers: ["jest-serializer-vue"]
+  transformIgnorePatterns: ["/node_modules/"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1"
+  },
+  snapshotSerializers: ["jest-serializer-vue"],
+  testMatch: [
+    "**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)"
+  ],
+  testURL: "http://localhost/",
+  watchPlugins: [
+    "jest-watch-typeahead/filename",
+    "jest-watch-typeahead/testname"
+  ],
+  globals: {
+    "ts-jest": {
+      babelConfig: true
+    }
+  }
 }
