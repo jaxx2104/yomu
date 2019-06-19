e849983753e72ed515f209561e22c9251e745357
jaxx2104 <jaxx2104@gmail.com>
2019-06-20 01:14:11 +0900

    :sparkles: Add Storybook + TypeScript

--- src/stories/Icon.stories.js
+++ src/stories/Icon.stories.js
@@ -1,18 +1,13 @@
 import { storiesOf } from "@storybook/vue"
 import { withKnobs } from "@storybook/addon-knobs"
-import Icon from "./Icon.vue"
+
+import Icon from "../components/Icon.vue"
 
 storiesOf("atoms", module)
   .addDecorator(withKnobs)
   .add("Icon", () => {
     return {
       components: { Icon },
-      template: `
-      <div>
-        <Icon icon="cancel"/>
-        <Icon icon="search"/>
-        <Icon icon="share"/>
-        <Icon icon="toggle"/>
-      </div>`
+      template: `<Icon icon="search"/>`
     }
   })
