8bbd13b7e25cc188b0bb71cf2496687435bd665b
jaxx2104 <jaxx2104@gmail.com>
2019-06-30 01:10:58 +0900

    :sparkles: Add GenerateSW

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const configureWebpack = require("./webpack.config")
+const path = require("path")
 
 module.exports = {
   configureWebpack,
@@ -9,6 +10,38 @@ module.exports = {
         pathRewrite: { "^/api": "" },
         changeOrigin: true
       }
+    }
+  },
+  pwa: {
+    // configure the workbox plugin
+    workboxPluginMode: "GenerateSW",
+    workboxOptions: {
+      swDest: path.join(__dirname, "/dist/service-worker.js"),
+      globDirectory: path.join(__dirname, "/dist/"),
+      globPatterns: [
+        "*.{html,js,css}",
+        "images/**/*.{jpg,jpeg,png,gif,webp,svg}"
+      ],
+      runtimeCaching: [
+        {
+          urlPattern: /.*firebase.*/,
+          handler: "networkFirst",
+          options: {
+            cacheName: "api"
+          }
+        },
+        {
+          urlPattern: /\.(png|svg|jpg|gif|svg|woff|ttf|eot)/,
+          handler: "cacheFirst",
+          options: {
+            cacheName: "assets",
+            cacheExpiration: {
+              maxAgeSeconds: 86400 * 7,
+              maxEntries: 1000
+            }
+          }
+        }
+      ]
     }
   },
   lintOnSave: false
